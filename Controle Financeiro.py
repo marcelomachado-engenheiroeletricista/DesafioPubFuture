044e589ffe87898d954bc01800352c825936e1ec
Marcelo Machado <marcelo_2016@outlook.com>
2022-01-04 19:15:50 -0300

    Cadastros gerais

--- Controle Financeiro.py
+++ Controle Financeiro.py
@@ -1,8 +1,11 @@
+########## Importanto bibliotecas ##########
 
-''' Ao reler o desafio, percebi que estava sendo solicitado banco de dados relacional porém havia entendido que
-estava sendo solicitado DB Não-Relacional (erro de interpretação), assim o código teve que ser reiniciado'''
+import pyodbc               # Biblioteca OBBC para conetar ao banco de dados SQl
+import pandas as pd         # Biblioteca Pandas p/ ler o banco de dados
 
-import pyodbc # Biblioteca do banco de dados SQl
+############################################
+
+########## Fazer conexão com o DB ##########
 
 dados_conexao = (
     "Driver={SQL Server};"
@@ -11,62 +14,61 @@ dados_conexao = (
 )
 
 conexao = pyodbc.connect(dados_conexao)
-
 cursor = conexao.cursor()
 
-'''def Cad_Rec():                                             # Irá Cadastrar uma nova receita
+############################################
+
+################ Funções ###################
+
+def Cad_Rec():                                             # Irá Cadastrar uma nova receita
+    print('Digite a Receita')
     rv = float(input('Valor R$: '))
     rdr = input('Data do recebimento: ')
-    rd = str(input('Descrição: '))
+    rd = str(input('Descrição: ').upper())
     rc = str(input('Conta: ').upper())
     rt = str(input('Tipo de Receita: ').upper())
-    Receita = {
-        'valor': rv,
-        'dataRecebimento': rdr,
-        'descrição': f'{rd}',
-        'conta': f'{rc}',
-        'tipoReceita': f'{rt}'
-    }
-    collection_rec.insert_one(Receita)
 
-    rv = valor
-    rdr = dataRecebimento
-    rd = descrição
-    rc = conta
-    rt = tipoReceita (salário, presente, prêmio, outros)
-
+    comando = f"""INSERT INTO RECEITAS(rvalor, rdata, rdescrição, rconta, rtipo)
+    VALUES
+        ({rv}, '{rdr}', '{rd}', '{rc}', '{rt}')"""
+    cursor.execute(comando)
+    cursor.commit()
+    # Não esquecer de atualizar saldo
 
 def Cad_Des():                                             # Irá Cadastrar uma nova despesa
+    print('Digite a Despesa')
     dv = float(input('Valor R$: '))
     ddr = input('Data do recebimento: ')
-    dd = str(input('Descrição: '))
+    dd = str(input('Descrição: ').upper())
     dc = str(input('Conta: ').upper())
     dt = str(input('Tipo de Receita: ').upper())
-    Despesa = {
-        'valor': dv,
-        'dataRecebimento': ddr,
-        'descrição': f'{dd}',
-        'conta': f'{dc}',
-        'tipoDespesa': f'{dt}'
-    }
-    collection_des.insert_one(Despesa)
 
-    dv = valor
-    ddr = data do Recebimento
-    dd = descrição
-    dc = conta
-    dt = tipo da Despesa (salário, presente, prêmio, outros
+    comando = f"""INSERT INTO DESPESAS(dvalor, ddata, ddescrição, dconta, dtipo)
+    VALUES
+        ({dv}, '{ddr}', '{dd}', '{dc}', '{dt}')"""
+    cursor.execute(comando)
+    cursor.commit()
+    #Não esquecer de atualizar saldo
 
-        rvalor decimal(10, 2),
-        rdata date,
-        rdescrição varchar(50),
-        rconta varchar(50),
-        rtipo varchar(50),
-        dvalor decimal(10, 2),
-        ddata date,
-        ddescrição varchar(50),
-        dconta varchar(50),
-        dtipo varchar(50),
-		csaldo decimal(10, 2),
-		ctipo varchar(50),
-		cintf varchar(50)'''
+def Cad_Con():                                             # Irá Cadastrar uma nova despesa
+    print('Digite os dados de sua Conta')
+    csaldo = float(input('Valor R$: '))
+    ctipo = str(input('Tipo de conta: ').upper())
+    cintf = str(input('Instituição Financeira: ').upper())
+
+    comando = f"""INSERT INTO CONTAS(csaldo, ctipo, cintf)
+    VALUES
+        ({csaldo }, '{ctipo}', '{cintf}')"""
+    cursor.execute(comando)
+    cursor.commit()
+
+############################################
+
+
+################## MAIN ####################
+
+Cad_Rec()
+Cad_Des()
+Cad_Con()
+
+############################################
