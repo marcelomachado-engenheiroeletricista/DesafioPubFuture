b0db7a572feec6b4ac757d0af1e1bc4402852d2f
Marcelo Machado <marcelo_2016@outlook.com>
2022-01-05 07:19:30 -0300

    Função Remover

--- Controle Financeiro.py
+++ Controle Financeiro.py
@@ -20,6 +20,24 @@ cursor = conexao.cursor()
 
 ################ Funções ###################
 
+def Inicial():
+    exe1 = input('Contas => (C) \n'
+                 'Receitas => (R) \n'
+                 'Despesas => (D) \n'
+                 'Sair => (S) \n'
+                 'Digite a opção que deseja executar conforme acima: ').upper()
+    while True:
+        if exe1 == 'C':
+            Contas()
+        elif exe1 == 'R':
+            Receitas()
+        elif exe1 == 'D':
+            Despesas()
+        elif exe1 == 'S':
+            break
+        else:
+            exe1 = input('Inválido, tente navamente: ').upper()
+
 
 # Funções referentes as Receitas
 
@@ -53,12 +71,12 @@ def Cad_Rec():                                             # Irá Cadastrar uma 
     rd = str(input('Descrição: ').upper())
     rc = str(input('Conta: ').upper())
     rt = str(input('Tipo de Receita: ').upper())
-
     comando = f"""INSERT INTO RECEITAS(rvalor, rdata, rdescrição, rconta, rtipo)
     VALUES
         ({rv}, '{rdr}', '{rd}', '{rc}', '{rt}')"""
     cursor.execute(comando)
     cursor.commit()
+    Inicial()
     # Não esquecer de atualizar saldo
 
 
@@ -66,12 +84,43 @@ def Ed_Rec():                                             # Irá Editar uma rece
     a = 'ok'
 
 
-def Rem_Rec():                                             # Irá Remover uma receita
-    a = 'ok'
+def Rem_Rec():                                             # Remover receitas
+    exeRR = input('Remover todas => (T) \n'
+                  'Especifica => (E) \n' 
+                  'Voltar => (V) \n' 
+                  'Sair => (S) \n'
+                  'Digite uma opção acima: ').upper()
+    while True:
+        if exeRR == 'T':
+            Rem_Rec_SQL = """DELETE FROM RECEITAS"""
+            cursor.execute(Rem_Rec_SQL)
+            cursor.commit()
+            Inicial()
+        elif exeRR == 'E':
+            conexao = pyodbc.connect(dados_conexao)
+            cursor = conexao.cursor()
+            Rec = """SELECT * FROM RECEITAS"""
+            listar = pd.read_sql_query(Rec, conexao)
+            print(listar)
+            esc = str(input('Digite a receita a ser removida: ')).upper()
+            escolher = f"""DELETE FROM RECEITAS WHERE rdescrição = '{esc}'"""
+            cursor.execute(escolher)
+            cursor.commit()
+            Inicial()
+            #NÃO ESQUECER DE ATUALIZAR O SALDO
+        elif exeRR == 'V':
+            Receitas()
+        elif exeRR == 'S':
+            break
+        else:
+            exeRR = input('Inválido, tente navamente: ').upper()
 
 
 def Lis_Rec():                                             # Irá Listar as receitas
-    a = 'ok'
+   ''' sql_Rec = """SELECT * FROM RECEITAS"""
+    Rec = pd.read_sql_query(sqlEXEC, conexao)
+    return Rec'''
+
 
 
 # Funções referentes as Despesas
@@ -105,16 +154,17 @@ def Despesas():
 def Cad_Des():                                             # Irá Cadastrar uma nova despesa
     print('Digite a Despesa')
     dv = float(input('Valor R$: '))
-    ddr = input('Data do recebimento: ')
+    ddr = input('Data do Pagamento: ')
     dd = str(input('Descrição: ').upper())
     dc = str(input('Conta: ').upper())
-    dt = str(input('Tipo de Receita: ').upper())
+    dt = str(input('Tipo de Despesa: ').upper())
 
     comando = f"""INSERT INTO DESPESAS(dvalor, ddata, ddescrição, dconta, dtipo)
     VALUES
         ({dv}, '{ddr}', '{dd}', '{dc}', '{dt}')"""
     cursor.execute(comando)
     cursor.commit()
+    Inicial()
     #Não esquecer de atualizar saldo
 
 
@@ -122,8 +172,34 @@ def Ed_Des():                                             # Irá Editar uma Desp
     a = 'ok'
 
 
-def Rem_Des():                                             # Irá Remover uma Despesa
-    a = 'ok'
+def Rem_Des():                                             # Remover Despesas
+    exeRD = input('Remover todas => (T) \n'
+                  'Especifica => (E) \n'
+                  'Voltar => (V) \n'
+                  'Sair => (S) \n'
+                  'Digite uma opção acima: ').upper()
+    while True:
+        if exeRD == 'T':
+            Rem_Des_SQL = """DELETE FROM DESPESAS"""
+            cursor.execute(Rem_Des_SQL)
+            cursor.commit()
+            Inicial()
+        elif exeRD == 'E':
+            Des = """SELECT * FROM DESPESAS"""
+            listar = pd.read_sql_query(Des, conexao)
+            print(listar)
+            esc = str(input('Digite a despesa a ser removida: ')).upper()
+            escolher = f"""DELETE FROM DESPESAS WHERE ddescrição = '{esc}'"""
+            cursor.execute(escolher)
+            cursor.commit()
+            Inicial()
+            # NÃO ESQUECER DE ATUALIZAR O SALDO
+        elif exeRD == 'V':
+            Despesas()
+        elif exeRD == 'S':
+            break
+        else:
+            exeRD = input('Inválido, tente navamente: ').upper()
 
 
 def Lis_Des():                                             # Irá Listar as Despesa
@@ -174,14 +250,41 @@ def Cad_Con():                                             # Irá Cadastrar uma 
         ({csaldo }, '{ctipo}', '{cintf}')"""
     cursor.execute(comando)
     cursor.commit()
+    Inicial()
 
 
 def Ed_Con():                                             # Irá Editar uma Despesa
     a = 'ok'
 
 
-def Rem_Con():                                             # Irá Remover uma Despesa
-    a = 'ok'
+def Rem_Con():                                             # Remover Contas
+    exeRC = input('Remover todas => (T) \n'
+                  'Especifica => (E) \n'
+                  'Voltar => (V) \n'
+                  'Sair => (S) \n'
+                  'Digite uma opção acima: ').upper()
+    while True:
+        if exeRC == 'T':
+            Rem_Con_SQL = """DELETE FROM CONTAS"""
+            cursor.execute(Rem_Con_SQL)
+            cursor.commit()
+            Inicial()
+        elif exeRC == 'E':
+            Con = """SELECT * FROM CONTAS"""
+            listar = pd.read_sql_query(Con, conexao)
+            print(listar)
+            esc = str(input('Digite a conta a ser removida: ')).upper()
+            escolher = f"""DELETE FROM CONTAS WHERE cintf = '{esc}'"""
+            cursor.execute(escolher)
+            cursor.commit()
+            Inicial()
+            # NÃO ESQUECER DE ATUALIZAR O SALDO
+        elif exeRC == 'V':
+            Contas()
+        elif exeRC == 'S':
+            break
+        else:
+            exeRD = input('Inválido, tente navamente: ').upper()
 
 
 def Lis_Con():                                             # Irá Listar as Despesa
@@ -202,21 +305,8 @@ def Lit_Con():                                             # Irá Remover uma De
 ################## MAIN ####################
 
 
-exe1 = input('Contas => (C) \n'
-             'Receitas => (R) \n'
-             'Despesas => (D) \n'
-             'Sair => (S) \n'
-             'Digite a opção que deseja executar conforme acima: ').upper()
-while True:
-    if exe1 == 'C':
-        Contas()
-    elif exe1 == 'R':
-        Receitas()
-    elif exe1 == 'D':
-        Despesas()
-    elif exe1 == 'S':
-        break
-    else:
-        exe1 = input('Inválido, tente navamente: ').upper()
+Inicial()
 
 ############################################
+
+
